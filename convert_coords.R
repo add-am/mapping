617af4a993b5515f294974ac3ccee418effd8a6a
Adam Shand <to@drytropicshealthywaters.org>
2022-11-24 17:27:13 +1000

    finalize regional ecosystem analysis

--- convert_coords.R
+++
@@ -1,9 +0,0 @@
-convert_coords = function(lat,long, from = CRS("+init=epsg:4326"), to) {
-  data = data.frame(long=long, lat=lat)
-  coordinates(data) <- ~ long+lat
-  proj4string(data) = from
-  #Convert to coordinate system specified by EPSG code
-  xy = data.frame(sp::spTransform(data, to))
-  colnames(xy) = c("x","y")
-  return(unlist(xy))
-}
