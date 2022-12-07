6cd5db6e65deb1a376ad56ff808e572ed23b6207
Adam Shand <to@drytropicshealthywaters.org>
2022-12-07 16:51:58 +1000

    Update to RE data preparation

--- test.R
+++ test.R
@@ -11,55 +11,45 @@ library(tidyverse)
 library(openxlsx2)
 
 
-#read in regional ecosystem data
-re_remnant <- st_read(dsn = "data/regional_ecosystems/re_remnant.gpkg")
-re_p_clear <- st_read(dsn = "data/regional_ecosystems/re_pre_clearing.gpkg")
+#assume crs is set by the quarto doc calling this function
+#proj_crs <- "EPSG:7844"
 
+#read in basins data
+basins <- sf::st_read(dsn = "data/shapefiles/Drainage_basins.shp")
 
-#set the crs
-proj_crs <- "EPSG:4283"
+#select northern three basins and combine into one large multipolygon
+n3_basins <- basins |> 
+  dplyr::filter(BASIN_NAME %in% c("Ross", "Black", "Don", "Proserpine", "O'Connell", "Pioneer", 
+                                  "Plane", "Daintree", "Mossman", "Barron", "Johnstone", "Tully", 
+                                  "Murray", "Herbert")) |> sf::st_union()
 
-#get path to geodatabase
-path <- "data/raw/historic_re_layers_v12_2/Regional_Ecosystem_v12_2_geo.gdb"
+#get path to layers
+path <- "data/raw/Regional_Ecosystem_Geopackage_Files/"
 
-#get list of layers in the geodatabase
-layers <- st_layers(dsn = path)
+#get list of files in the folder without their extension
+file_list <- tools::file_path_sans_ext(list.files(path))
 
-#for each item in the list
-for (i in 1:length(layers$name)){
-  
-  #load the layer
-  loaded_layer <- st_read(path, layer = layers$name[i])
-  
-  #transform the crs to GDA2020 (the new aus crs)
-  #loaded_layer <- st_transform(loaded_layer, proj_crs)
+#for each file in list
+for (i in 1:length(file_list)){
+
+  #read in the regional ecosystem layer
+  re_layer <- st_read(dsn = glue("{path}{file_list[i]}.gpkg"))
   
   #filter for row with relevant RE's
-  #loaded_layer <- loaded_layer |> filter(str_detect(RE, c("7.\\d.\\d|8.\\d.\\d|11.\\d.\\d")))
+  #re_layer <- re_layer |> filter(str_detect(RE, c("7.\\d.\\d|8.\\d.\\d|11.\\d.\\d")))
+  
+  #transform the layer
+  re_layer <- st_transform(re_layer, proj_crs)
   
   #create a T F list of polygons that intersect the n3 basins
-  intersects <- lengths(sf::st_intersects(loaded_layer, n3_basins)) > 0
+  intersects <- lengths(sf::st_intersects(re_layer, n3_basins)) > 0
   
   #select only rows with T for intersection
-  loaded_layer <- data |> dplyr::mutate(within = intersects) |> dplyr::filter(within == T)
+  re_layer <- re_layer |> dplyr::mutate(within = intersects) |> dplyr::filter(within == T)
   
-  #save the file as a geopackage (this just makes the next steps easier)
-  st_write(loaded_layer, dsn = glue("{(layers$name)[i]}.gpkg"))
-  
+  #save the file 
+  sf::st_write(re_layer, dsn = glue("data/regional_ecosystems/{file_list[i]}_cropped.gpkg"),
+               delete_dsn = T)
 }
 
 
-
-test2 <- st_read(dsn = "data/raw/historic_re_layers_v12_2/re_remnant_2019_v12_2.gpkg")
-
-
-
-list_of_features[[1]] <- st_transform(list_of_features[[1]], proj_crs)
-
-#save
-st_write(list_of_features[[1]], dsn = "data/raw/historic_re_layers_v12_2/test.gpkg")
-
-
-
-#create a T F list of polygons that intersect the n3 basins
-intersects <- lengths(sf::st_intersects(list_of_features[[1]], n3_basins)) > 0
